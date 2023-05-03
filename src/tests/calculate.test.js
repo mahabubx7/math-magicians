a97f7e571ec4b7f169a3c20832e074c4f35aa727
Mahabub Alam <mahabubx7@gmail.com>
2023-05-03 22:01:14 +0600

    :rocket: tests:added-calculate-unit-tests

---
+++ src/tests/calculate.test.js
@@ -0,0 +1,40 @@
+import calculate from '../logic/calculate';
+
+describe('Unit tests for calculate.js', () => {
+  test('should return an object', () => {
+    expect(typeof calculate({}, 'AC')).toStrictEqual('object');
+  });
+
+  test('should return object with "total: 0" by AC button', () => {
+    expect(calculate({}, 'AC')).toStrictEqual({
+      total: 0,
+      next: null,
+      operation: null,
+    });
+  });
+
+  test('should return number by number buttons', () => {
+    const obj = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
+    expect(calculate(obj, '7')).toStrictEqual({
+      total: null,
+      next: '7',
+    });
+  });
+
+  test('should return calculated result on demand', () => {
+    const obj = {
+      total: 5,
+      next: 7,
+      operation: '+',
+    };
+    expect(calculate(obj, '=')).toStrictEqual({
+      total: '12',
+      next: null,
+      operation: null,
+    });
+  });
+});
